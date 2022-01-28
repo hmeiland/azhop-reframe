1bfdec180b95128467154e578ea1d6acbb294a67
clusteruser <clusteruser@ondemand.5k4koihasqqeflwka5a2cmpwme.ax.internal.cloudapp.net>
2022-01-28 20:09:04 +0000

    stream test for hc44rs

---
+++ config/settings.py
@@ -0,0 +1,69 @@
+site_configuration = {
+    'systems': [
+        {
+            'name': 'azhop',
+            'descr': 'Azure HPC Ondemand with PBS',
+            'modules_system': 'lmod',
+	        'hostnames': ['ondemand'],
+            'partitions': [
+                {
+                    'name': 'hc44rs',
+                    'scheduler': 'pbs',
+                    'access': ['slot_type=hc44rs'],
+                    'launcher': 'local',
+                    'modules': ['foss/2020a'],
+                    'environs': ['builtin'],
+                    'processor': {
+                        'num_cpus': 44,
+                    },
+                    'descr': 'HC44rs vm'
+                },
+             ]
+         },
+     ],
+    'environments': [
+        {
+            'name': 'builtin',
+            'cc': 'gcc',
+            'cxx': '',
+            'ftn': '',
+        },
+     ],
+     'logging': [
+        {
+            'level': 'debug',
+            'handlers': [
+                {
+                    'type': 'stream',
+                    'name': 'stdout',
+                    'level': 'info',
+                    'format': '%(message)s'
+                },
+                {
+                    'type': 'file',
+                    'name': 'reframe.log',
+                    'level': 'debug',
+                    'format': '[%(asctime)s] %(levelname)s: %(check_info)s: %(message)s',   # noqa: E501
+                    'append': False
+                }
+            ],
+            'handlers_perflog': [
+                {
+                    'type': 'filelog',
+                    'prefix': '%(check_system)s/%(check_partition)s',
+                    'level': 'info',
+                    'format': (
+                        '%(check_job_completion_time)s|reframe %(version)s|'
+                        '%(check_info)s|jobid=%(check_jobid)s|'
+                        '%(check_perf_var)s=%(check_perf_value)s|'
+                        'ref=%(check_perf_ref)s '
+                        '(l=%(check_perf_lower_thres)s, '
+                        'u=%(check_perf_upper_thres)s)|'
+                        '%(check_perf_unit)s'
+                    ),
+                    'append': True
+                }
+            ]
+        }
+    ],
+}
